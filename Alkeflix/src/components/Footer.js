1dc92da4cdb87af92e9cda327d937de68f31b0ad
iPizza <ianfigueroa507@gmail.com>
2022-05-25 14:59:01 -0300

    añadiendo alkeflix

--- Alkeflix/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer className="d-flex justify-content-center bg-dark">
-      <p className="text-light m-5">Copyright Alekemy Challenge</p>
-    </footer>
-  );
-}
-export default Footer;
